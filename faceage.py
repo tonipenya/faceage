6bda8edb5f7f70645746854fe5f44396a9ce02eb
Toni Penya-Alba <toni.penya.alba@gmail.com>
2015-06-08 10:56:03 +0200

    getting a response for an image on the internet

---
+++ faceage.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+if __name__ == '__main__':
+    keyFile = open('AZUREKEY.txt', 'r')
+    PROJECT_OXFORD_KEY = keyFile.readline()
+    print PROJECT_OXFORD_KEY
+
+    # https://is.azure-api.net/face/v0/detections[?analyzesAge][&analyzesGender][&analyzesFaceLandmarks][&analyzesHeadPose]&subscription-key=<Your subscription key>
+    url = 'https://api.projectoxford.ai/face/v0/detections?subscription-key=' + PROJECT_OXFORD_KEY
+    files = {'file': open('toni0.jpg', 'rb')}
+
+    payload = { 'url': 'https://binarymaxsum.github.io/img/tonipenya.png' }
+    r = requests.post(url, data=json.dumps(payload))
+    print r.text
+
