32f163f82fbd20102be83f1b1c471133d82cd6eb
Toni Penya-Alba <toni.penya.alba@gmail.com>
2015-06-08 11:00:36 +0200

    printing age and gender

--- faceage.py
+++ faceage.py
@@ -7,10 +7,11 @@ if __name__ == '__main__':
     print PROJECT_OXFORD_KEY
 
     # https://is.azure-api.net/face/v0/detections[?analyzesAge][&analyzesGender][&analyzesFaceLandmarks][&analyzesHeadPose]&subscription-key=<Your subscription key>
-    url = 'https://api.projectoxford.ai/face/v0/detections?subscription-key=' + PROJECT_OXFORD_KEY
+    url = 'https://api.projectoxford.ai/face/v0/detections?analyzesAge=true&analyzesGender=true&subscription-key=' + PROJECT_OXFORD_KEY
     files = {'file': open('toni0.jpg', 'rb')}
 
     payload = { 'url': 'https://binarymaxsum.github.io/img/tonipenya.png' }
     r = requests.post(url, data=json.dumps(payload))
-    print r.text
+    print json.loads(r.text)[0]['attributes']
+
 
